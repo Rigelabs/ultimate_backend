490ad69f86669284b41644ef2c3fa23653f8cfef
rigelsolutions@outlook.com <rigelsolutions@outlook.com>
2021-10-11 13:09:49 +0300

    enabled https with certs obtained from ubuntu vm

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const helmet= require('helmet');
+const helmet = require('helmet');
 const morgan = require('morgan');
 const cors = require('cors');
-const env =require('dotenv');
+const env = require('dotenv');
 const path = require('path');
 const logger = require('./middlewares/logger');
-const rfs =require("rotating-file-stream");
-const generalLimiter = require('./middlewares/rateLimiters/genericLimiter')
+const rfs = require("rotating-file-stream");
+const https = require('https');
+const fs = require('fs');
 
-const process =require('process');
+const process = require('process');
 
 
-const app=express()
+const app = express()
 // Cross-origin resource sharing (CORS) is a mechanism that allows 
 //restricted resources on a web page to be requested from another domain outside the domain from which the first resource was served
 app.use(cors());
@@ -20,7 +21,7 @@ app.use(cors());
 
 //initialize bodyparser
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
 //environment variable
 env.config();
@@ -33,93 +34,96 @@ app.use(helmet());
 
 //initialize morgan for server logging
 // create a write stream that rotates each day and when 10mb of data then compresses the rotated files
-const serverLogStream =rfs.createStream("morganLogs.log",{
-    size: "10M", // rotate every 10 MegaBytes written
-    interval: "1d", // rotate daily
-    compress: "gzip", // compress rotated files
-    path: path.join(__dirname, 'logs')
-    
-  });
+const serverLogStream = rfs.createStream("morganLogs.log", {
+  size: "10M", // rotate every 10 MegaBytes written
+  interval: "1d", // rotate daily
+  compress: "gzip", // compress rotated files
+  path: path.join(__dirname, 'logs')
 
- app.use(morgan(':date[web] :status :method :url :remote-addr :remote-user :total-time :user-agent',{stream:serverLogStream}));
+});
 
- 
-const PORT =process.env.PORT || 5000
+app.use(morgan(':date[web] :status :method :url :remote-addr :remote-user :total-time :user-agent', { stream: serverLogStream }));
 
-app.listen(PORT,()=>{
-    console.log(`Server running in ${process.env.NODE_ENV} on port  ${PORT}`),
+//Setting up https server
+const key = fs.readFileSync("./key.pem");
+const cert = fs.readFileSync('./cert.pem');
+
+const PORT = process.env.PORT || 5000
+const server = https.createServer({ key: key, cert: cert }, app);
+server.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} on port  ${PORT}`),
     logger.info(`Server running in ${process.env.NODE_ENV} on port  ${PORT}`)
-    })
+})
 
 
-const refreshRouter= require('./routes/refreshToken')
-const authRouter= require('./routes/auth')
+const refreshRouter = require('./routes/refreshToken')
+const authRouter = require('./routes/auth')
 
-app.use('/api',refreshRouter);
-app.use('/api',authRouter);
+app.use('/api', refreshRouter);
+app.use('/api', authRouter);
 
 app.get('/', function (req, res) {
-        res.send('hello, world!')
-        logger.info(`${res.statusMessage} - ${req.originalUrl} - ${req.method} - ${req.ip}`)
-      })
+  res.send('hello, world!')
+  logger.info(`${res.statusMessage} - ${req.originalUrl} - ${req.method} - ${req.ip}`)
+})
 
 
 // Capture 500 errors
-app.use((err,req,res,next) => {
-res.status(500).send('Server Error!');
-   logger.error(`${err.status || 500} - ${res.statusMessage} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+app.use((err, req, res, next) => {
+  res.status(500).send('Server Error!');
+  logger.error(`${err.status || 500} - ${res.statusMessage} - ${err.message} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
 })
 
 // Capture 404 erors
-app.use((req,res,next) => {
-    res.status(404).send("PAGE NOT FOUND");
-    logger.error(`404 || ${res.statusMessage} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+app.use((req, res, next) => {
+  res.status(404).send("PAGE NOT FOUND");
+  logger.error(`404 || ${res.statusMessage} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
 })
 
 
 
 //setting up db
-const dbURL=process.env.MONGO_URI
-mongoose.connect(dbURL,{useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex: true}).then((result,err)=>{
-         if(err){
-            console.log(err)
+const dbURL = process.env.MONGO_URI
+mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }).then((result, err) => {
+  if (err) {
+    console.log(err)
 
-         }
-         if(result){
-            console.log('Connected to DB'),logger.info("Connected to DB")
-         }
+  }
+  if (result) {
+    console.log('Connected to DB'), logger.info("Connected to DB")
+  }
 });
-mongoose.connection.on('disconnected',err=>{
-    console.log('DB disconnected'),
+mongoose.connection.on('disconnected', err => {
+  console.log('DB disconnected'),
     logger.error('DB disconnected')
 })
-mongoose.connection.on('reconnected',err=>{
-    console.log('DB reconnected'),
+mongoose.connection.on('reconnected', err => {
+  console.log('DB reconnected'),
     logger.info('DB reconnected')
 })
-mongoose.connection.on('error',err=>{
-    console.log('DB Error',err),
-    logger.error('DB Error',err)
+mongoose.connection.on('error', err => {
+  console.log('DB Error', err),
+    logger.error('DB Error', err)
 })
 
 //uncaughtException to crash the nodejs process
 process.on('unhandledRejection', (err, origin) => {
-    logger.error('Unhandled rejection at ', origin, `reason: ${err}`)
-    console.log('Unhandled rejection at ', origin, `reason: ${err}`)
-    
-  })  
+  logger.error('Unhandled rejection at ', origin, `reason: ${err}`)
+  console.log('Unhandled rejection at ', origin, `reason: ${err}`)
+
+})
 //The 'rejectionHandled' event is emitted whenever a Promise has been rejected
 // and an error handler was attached to it (using promise.catch(), for example) later than one turn of the Node.js event loop.
 process.on('rejectionHandled', (err, origin) => {
-    logger.error('RejectionHandled at ', origin, `reason: ${err}`)
-    console.log('RejectionHandled at ', origin, `reason: ${err}`)
-  
-  })  
-//The 'exit' event is emitted when the Node.js process is about to exit as a result of either:
-  //The process.exit() method being called explicitly;
-  //The Node.js event loop no longer having any additional work to perform.
-process.on('exit', (err, origin) => {
-    logger.error('Process Exited !! ', origin, `reason: ${err}`)
-    console.log('Process Exited !! ', origin, `reason: ${err}`)
+  logger.error('RejectionHandled at ', origin, `reason: ${err}`)
+  console.log('RejectionHandled at ', origin, `reason: ${err}`)
 
-  })  
+})
+//The 'exit' event is emitted when the Node.js process is about to exit as a result of either:
+//The process.exit() method being called explicitly;
+//The Node.js event loop no longer having any additional work to perform.
+process.on('exit', (err, origin) => {
+  logger.error('Process Exited !! ', origin, `reason: ${err}`)
+  console.log('Process Exited !! ', origin, `reason: ${err}`)
+
+})
